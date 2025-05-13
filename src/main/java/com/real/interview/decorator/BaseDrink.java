e157de7c2628343f03842a6c2864447e9301583e
Aditya Velala <veladitya@gmail.com>
2025-05-13 16:56:10 -0500

    Observer

---
+++ src/main/java/com/real/interview/decorator/BaseDrink.java
@@ -0,0 +1,18 @@
+package com.real.interview.decorator;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Component("baseDrink")
+@Primary
+public class BaseDrink implements Beverage {
+    @Override
+    public String getDescription() {
+        return "BaseDrink";
+    }
+
+    @Override
+    public double cost() {
+        return 10.0;
+    }
+}
---
+++ src/main/java/com/real/interview/decorator/Beverage.java
@@ -0,0 +1,7 @@
+package com.real.interview.decorator;
+
+public interface Beverage {
+    String getDescription();
+
+    double cost();
+}
